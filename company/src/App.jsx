4ed171ba977094fb9968c742b9e663096b1e55ed
Vishnu642 <dkvishnuvardhan94@gmail.com>
2022-05-23 16:51:14 +0530

    commit

--- company/src/App.jsx
+++ company/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import axios from "axios";
-import Button from "./components/Button";
+// import Button from "./components/Button";
 import CandidateCard from "./components/CandidateCard";
 
 
@@ -37,10 +37,10 @@ function App() {
     <div className="App">
       <div>
        {loading && <div  id="loading-container">...Loading</div>}
-         <Button id="SORT_BUTTON" handleclick={setSalarySort("DESC")}  title={`Sort by Ascending Salary`} />
-        <Button title="PREV" onClick={()=>setPage(page-1)}  id="PREV" />
-        <Button id="NEXT" onClick={()=>setPage(page+1)} title="NEXT" /> 
-        
+         
+       <button className='button-1' onClick={()=>setSalarySort("DESC")} >{salarySort==="ASC"?"Sort by Descending Order":"Sort by Ascending Order"}</button>
+       <button className='button-1' disabled={page===1} onClick={()=>setPage(page-1)} >prev</button>
+        <button className='button-1'  onClick={()=>setPage(page+1)} >Next</button> 
   
       </div>
       {data.map((item) => (
